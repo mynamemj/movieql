6edf9b83a5321cba7434b841bc6aad5248c467e0
mynamemj <dlaudwp1313@gmail.com>
2019-04-19 19:18:23 +0900

    Rendering Home and Details Route with params

--- graphql/db.js
+++ graphql/db.js
@@ -1,35 +1,33 @@
 import axios from 'axios';
 const BASE_URL="https://yts.am/api/v2/";
 const LIST_MOVIES_URL = `${BASE_URL}list_movies.json`;
-const MOVIE_DATAILS_URL = `${BASE_URL}movie_datails.json`;
+const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json`;
 const MOVIE_SUGGESIONS_URL = `${BASE_URL}movie_suggestions.json`;
 // {
 //     data:{
 //         movies:[
-//           {},{}  ....
+//           {}  ....
 //         ]
 //     }
 // }
 
 export const getMovies = async(limit,rating)=> {
-    const{
-        data:{
-            data:{movies}
-        }
-    }= await axios(LIST_MOVIES_URL,{
+    
+    const data= await axios(LIST_MOVIES_URL,{
         params:{
             limit,
             minimum_rating:rating
         }
     });
-    return movies;
+    console.log(data);
+    return data.data.data.movies;
 };
 export const getMovie = async id =>{
     const {
         data:{
             data:{movie}
         }
-    }= await axios(MOVIE_DATAILS_URL,{
+    }= await axios(MOVIE_DETAILS_URL,{
         params:{
             movie_id:id
         }
